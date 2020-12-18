f0f6d5c8b73af5bcef801ba22f37a50212e381e7
shermosa <shermosa@hotels.com>
2020-12-18 15:14:34 +0000

    adding

--- src/main/java/com/hotels/mutantswarm/annotation/MutantSwarmTest.java
+++ src/main/java/com/hotels/mutantswarm/annotation/MutantSwarmTest.java
@@ -19,12 +19,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestTemplate;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @TestTemplate
-@Test
 public @interface MutantSwarmTest {
 }
